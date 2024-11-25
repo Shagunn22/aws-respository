36c698d0909ce41b1fd7faabbd8708ea831cf159
Shagunn22 <sharma.shagun0022@gmail.com>
2024-11-25 10:51:15 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,40 @@
+variable "s3_bucket_name" {
+  default     = "shagun123"
+  description = "Name of the s3 bucket"
+  type        = string
+}
+variable "s3_tags" {
+  type = map
+
+  default = {
+    createdby   = "shagun"
+    environment = "dev"
+  }
+}
+
+variable "s3_regions" {
+  type    = string
+  default = "eu-west-1"
+}
+
+
+variable "private_key_path"{
+  type = string
+  default = "./terraformkp.pem"
+}
+variable "key_name" {
+  type = string
+ description = "key name"
+ default = "Neeharika_Terraform"
+}
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+  description = "Name of the region to create resource"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet_address_space" {
+  default = "10.1.0.0/24"
+}
