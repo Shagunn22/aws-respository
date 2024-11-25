5d2af2ed4727fd23f49c54e571f31603acf26f29
Shagunn22 <sharma.shagun0022@gmail.com>
2024-11-25 11:08:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
- bucket = "shagun123"
+ bucket = "shagun25"
  key = "globalstate/s3/terraform.tfstate"
  region = "eu-west-1"
  # Replace this with your DynamoDB table name!
